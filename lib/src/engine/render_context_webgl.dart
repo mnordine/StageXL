203eb319f36fd4b5664f3d994f75a5ef2bc2d41c
Mark Nordine <mnordine@wooplagaming.com>
2020-05-18 17:23:34 -0300

    Fix bug where IE was broke because it didn't try experimental-webgl

--- lib/src/engine/render_context_webgl.dart
+++ lib/src/engine/render_context_webgl.dart
@@ -38,15 +38,20 @@ class RenderContextWebGL extends RenderContext {
     _canvasElement.onWebGlContextLost.listen(_onContextLost);
     _canvasElement.onWebGlContextRestored.listen(_onContextRestored);
 
-    var renderingContext = _canvasElement.getContext('webgl', {
+    var options = {
       'alpha': alpha,
-      'antialias': antialias,
       'depth': false,
       'stencil': true,
+      'antialias': antialias,
       'premultipliedAlpha': true,
       'preserveDrawingBuffer': false,
       'powerPreference': 'high-performance',
-    }) as gl.RenderingContext;
+    };
+
+    var renderingContext = _canvasElement.getContext('webgl', options);
+    if (renderingContext == null) {
+      renderingContext = _canvasElement.getContext('experimental-webgl', options);
+    }
 
     if (renderingContext is! gl.RenderingContext) {
       throw new StateError("Failed to get WebGL context.");
