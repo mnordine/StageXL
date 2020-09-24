34a26abc5d85ee1b70336d6a462e029dea0eb370
Mark Nordine <mnordine@wooplagaming.com>
2020-09-24 11:17:58 -0300

    Remove debug statements

--- lib/src/resources.dart
+++ lib/src/resources.dart
@@ -44,29 +44,23 @@ String getUrlHash(String url, {bool webp = false}) {
   if (stageXLFileMap == null) return url;
 
   if (webp) {
-    print('getting webp instead of $url...');
-
     // This is a hack, since it will break if the hash format changes.
     final i = url.lastIndexOf('-');
     final j = url.lastIndexOf('@');
     url = url.substring(0, i) + url.substring(j);
-    print('normalized url to $url');
 
     var match = RegExp(r'(png|jpg|jpeg)$').firstMatch(url);
     url = url.substring(0, match.start) + 'webp';
-    print('new key: $url');
     return getUrlHash(url);
   }
 
   final key = url.replaceFirst(stageXLStoragePrefix, '');
   final value = stageXLFileMap[key];
   if (value == null) {
-    print('$url not found in file map from key: $key');
     return null;
   }
 
   final newUrl = '$stageXLStoragePrefix${stageXLFileMap[key] as String}';
-  print('getting url, key: $key, value: $value, new url: $newUrl');
   return newUrl;
 }
 
