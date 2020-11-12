d6fe6583294ba83cf544aac0474b6f184820be2a
Mark Nordine <mnordine@wooplagaming.com>
2020-11-12 11:49:19 -0400

    Add optional onComplete to addTranslation

--- lib/src/animation/juggler.dart
+++ lib/src/animation/juggler.dart
@@ -296,9 +296,11 @@ class Juggler implements Animatable {
   ///     });
 
   Translation addTranslation(num startValue, num targetValue, num time,
-      TransitionFunction transition, void Function(num value) onUpdate) {
+      TransitionFunction transition, void Function(num value) onUpdate,
+      {void Function()? onComplete}) {
     var translation = Translation(startValue, targetValue, time, transition);
     translation.onUpdate = onUpdate;
+    if (onComplete != null) translation.onComplete = onComplete;
     add(translation);
     return translation;
   }
