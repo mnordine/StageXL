2645969a48d0a6dfab45b759c5869a8a16a355ef
Mark Nordine <marknordine@gmail.com>
2021-05-02 13:38:01 -0300

    Add null checks in web audio api mixer, make _volumeNode final

--- lib/src/media/implementation/web_audio_api_mixer.dart
+++ lib/src/media/implementation/web_audio_api_mixer.dart
@@ -4,18 +4,19 @@ class WebAudioApiMixer {
   static final AudioContext audioContext = AudioContext();
 
   AudioNode _inputNode;
-  GainNode _volumeNode;
+  final GainNode _volumeNode;
 
-  WebAudioApiMixer([AudioNode inputNode]) {
+  WebAudioApiMixer([AudioNode inputNode]) : _volumeNode = audioContext.createGain() {
     _inputNode = inputNode ?? audioContext.destination;
-    _volumeNode = audioContext.createGain();
-    _volumeNode.connectNode(_inputNode);
+    if (_inputNode != null) _volumeNode.connectNode(_inputNode);
   }
 
   void applySoundTransform(SoundTransform soundTransform) {
+    if (audioContext.currentTime == null) return;
+
     var time = audioContext.currentTime;
     var value = pow(soundTransform.volume, 2);
-    _volumeNode.gain.setValueAtTime(value, time);
+    _volumeNode.gain?.setValueAtTime(value, time);
   }
 
   AudioNode get inputNode => _volumeNode;
